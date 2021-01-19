5c0edac20006ec050286086dc69731cc2d69cfa4
MilenaV <milena.n.valcheva@gmail.com>
2021-01-19 18:26:38 +0200

    End News Page

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-$(document).ready(function() {
+$(document).ready(function () {
     var swiper = new Swiper('.swiper-container', {
         slidesPerView: 3,
         spaceBetween: 30,
@@ -12,31 +12,28 @@ $(document).ready(function() {
             disableOnInteraction: true,
         },
         breakpoints: {
-            440: {
+            375: {
                 slidesPerView: 1,
                 spaceBetweenSlides: 0
             },
-            // when window width is <= 576px
             576: {
                 slidesPerView: 1,
                 spaceBetweenSlides: 0
             },
-                // when window width is <= 768px
-                768: {
-                    slidesPerView: 2,
-                    spaceBetweenSlides: 10
-                },
-                // when window width is <= 992px
-                992: {
-                    slidesPerView: 3,
-                    spaceBetweenSlides: 30
-                }
+            768: {
+                slidesPerView: 2,
+                spaceBetweenSlides: 10
+            },
+            992: {
+                slidesPerView: 3,
+                spaceBetweenSlides: 30
+            }
         }
     });
-  
-    $('ul.navbar-nav > li').click(function(e) {
-      e.preventDefault();
-      $('ul.navbar-nav > li').removeClass('active');
-      $(this).addClass('active');
+
+    $('ul.navbar-nav > li').click(function (e) {
+        e.preventDefault();
+        $('ul.navbar-nav > li').removeClass('active');
+        $(this).addClass('active');
     });
-  });
+});
